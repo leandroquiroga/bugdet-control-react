d42e95da1caf7c24efc24fe5557d8a6a83745a63
Leandro <leandroquiroga9514@gmail.com>
2022-01-23 23:55:12 -0300

    Src | Init App

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../node_modules/normalize.css/normalize.css'
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ControlGastos } from './ControlGastos';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ControlGastos />
   </React.StrictMode>,
   document.getElementById('root')
 );
