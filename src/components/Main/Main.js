0eaf7df32ebfa85ed3ad970a008c2410c69f2db4
Leandro <leandroquiroga9514@gmail.com>
2022-01-27 23:16:12 -0300

    Has been fixed the structure of this component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,55 +2,71 @@ import React from 'react';
 import { ListadoGastos } from './components/ListadoGastos';
 import { Modal } from './components/Modal';
 import iconSVGNewExpense from '../../img/nuevo-gasto.svg';
+import { FilterCategory } from './components/FilterCategory';
 
 
 export const Main = (props) => {
-    const {
-        animateModal,
-        modal,
-        setModal,
-        setAnimateModal,
-        saveExpenses,
-        arrExpenses,
-        setExpenseEdit,
-        expenesEdit,
-        deleteExpenses,
-    } = props;
-    
-    // Activa el modal y aploca la animacion correspondiente.
-    const handleNewExpenses = () => {
-        setModal(true);
-        setExpenseEdit({})
 
-        setTimeout(() => {
-            setAnimateModal(true);
-        }, 800);
-    }
-    return (
-        <main>
-            <ListadoGastos
-                arrExpenses={arrExpenses} 
-                setExpenseEdit={setExpenseEdit}
-                deleteExpenses={deleteExpenses }
-            /> 
-            <section className='nuevo-gasto'>
-                <img
-                    src={iconSVGNewExpense}
-                    alt='icon new expense'
-                    onClick={handleNewExpenses}
-                />    
-            </section>   
+  const {
+    animateModal,
+    modal,
+    setModal,
+    setAnimateModal,
+    saveExpenses,
+    arrExpenses,
+    setExpenseEdit,
+    expenesEdit,
+    deleteExpenses,
+    percentage,
+    filterCategory,
+    setFilterCategory,
+    filterExpenses,
+  } = props;
+  
+  // Activa el modal y aploca la animacion correspondiente.
+  const handleNewExpenses = () => {
+    setModal(true);
+    setExpenseEdit({})
 
-            {modal &&
-                <Modal
-                    animateModal={animateModal}
-                    setModal={setModal}
-                    setAnimateModal={setAnimateModal}
-                    saveExpenses={saveExpenses}
-                    expenesEdit={expenesEdit}
-                    setExpenseEdit={setExpenseEdit}
-                />
-            }
-        </main>
+    setTimeout(() => {
+        setAnimateModal(true);
+    }, 800);
+  }
+  return (
+    <main>
+      <FilterCategory
+        filterCategory={filterCategory}
+        setFilterCategory={setFilterCategory}
+      />
+      <ListadoGastos
+        arrExpenses={arrExpenses} 
+        setExpenseEdit={setExpenseEdit}
+        deleteExpenses={deleteExpenses}
+        filterExpenses={filterExpenses}
+        filterCategory={filterCategory}
+      /> 
+
+
+      {  (percentage < 100) && 
+          <section className='nuevo-gasto'>
+            <img
+              src={iconSVGNewExpense}
+              alt='icon new expense'
+              onClick={handleNewExpenses}
+            />    
+          </section>   
+      }
+
+      {modal &&
+        <Modal
+          animateModal={animateModal}
+          setModal={setModal}
+          setAnimateModal={setAnimateModal}
+          saveExpenses={saveExpenses}
+          expenesEdit={expenesEdit}
+          setExpenseEdit={setExpenseEdit}
+        />
+      }
+      </main>
     );
 };
